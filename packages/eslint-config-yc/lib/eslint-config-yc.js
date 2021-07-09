8dd7cd8d73c9ad0b40b051073c6b06ac2151fe57
vijay-ezee <vijay.reshamwala@ezeetechnosys.com>
2021-07-09 12:46:46 +0530

    removed plugin option

--- packages/eslint-config-yc/lib/eslint-config-yc.js
+++ packages/eslint-config-yc/lib/eslint-config-yc.js
@@ -2,9 +2,6 @@
 
 module.exports = {
     root: true,
-    plugins: [
-        '@typescript-eslint',
-      ],
       extends: [
         'plugin:@typescript-eslint/recommended',
         '@nuxtjs/eslint-config-typescript',
