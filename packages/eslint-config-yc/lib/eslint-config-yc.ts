c9e91d21f7313412dd31e86a8c26145b38e032a4
Piyush Goyani <piyush.goyani@ezeetechnosys.com>
2022-05-05 15:20:30 +0530

    refactor: migrate package files

--- packages/eslint-config-yc/lib/eslint-config-yc.ts
+++ packages/eslint-config-yc/lib/eslint-config-yc.ts
@@ -1,3 +1,9 @@
-export function eslintConfigYc(): string {
-  return 'eslint-config-yc';
+'use strict';
+
+module.exports = {
+  root: true,
+  extends: ['plugin:@typescript-eslint/recommended', '@nuxtjs/eslint-config-typescript'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
 }
